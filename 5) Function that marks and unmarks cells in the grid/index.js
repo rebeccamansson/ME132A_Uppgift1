a400313a06599ff7613bdf855edcdbf99d022397
rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>
2022-02-04 14:06:57 +0100

    done with code in part 5

--- 5) Function that marks and unmarks cells in the grid/index.js
+++ 5) Function that marks and unmarks cells in the grid/index.js
@@ -52,3 +52,59 @@ the classList.
 */
 
 
+let valueRows = document.querySelector("#inputRows").value;
+let valueCols = document.querySelector("#inputCols").value;
+
+
+
+function gridMaker(gridContainer, R, C) {
+    gridContainer.style.display = "grid";
+    gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`;
+
+    gridContainer.innerHTML = "";
+
+
+    for (let i = 0; i < C; i++) {
+
+
+        for (let j = 0; j < R; j++) {
+            gridContainer.appendChild(createNumberDiv());
+
+
+        };
+    };
+};
+
+
+
+console.log(gridMaker(document.querySelector("#grid"), valueRows, valueCols));
+
+function createNumberDiv() {
+    let divs = document.createElement("div");
+    divs.innerHTML = Math.floor(99 * Math.random());
+    divs.addEventListener("click", function () {
+    numberDiv.classList.toggle("selected");
+    updateResults("selected");
+    });
+
+
+    return divs;
+
+
+};
+
+console.log(gridMaker(document.querySelector("#grid"), valueRows, valueCols));
+
+
+
+
+
+
+
+
+
+
+
+
+
