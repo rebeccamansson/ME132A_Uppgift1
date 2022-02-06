5b6bcebdee5c8b41cd158556ce1bae1033e47df5
rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>
2022-02-06 13:05:26 +0100

    done with code in part 6

--- 6) CSS formatting/index.js
+++ 6) CSS formatting/index.js
@@ -8,3 +8,67 @@ You're free to format this as you want, but I expect some effort. See my intial 
 to get a sense of the level of formatting expected.
 
 */
+
+let valueRows = document.querySelector("#inputRows").value;
+let valueCols = document.querySelector("#inputCols").value;
+
+
+
+function gridMaker(gridContainer, R, C) {
+    gridContainer.style.display = "grid";
+    gridContainer.style.gridTemplateRows = `repeat(${R}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`;
+
+    gridContainer.innerHTML = "";
+
+
+    for (let i = 0; i < C; i++) {
+
+
+        for (let j = 0; j < R; j++) {
+            gridContainer.appendChild(createNumberDiv());
+
+
+        };
+    };
+};
+
+
+
+console.log(gridMaker(document.querySelector("#grid"), valueRows, valueCols));
+
+function createNumberDiv() {
+    let divs = document.createElement("div");
+    divs.innerHTML = Math.floor(99 * Math.random());
+    divs.addEventListener("click", function () {
+    divs.classList.toggle("selected");
+    updateResults("selected");
+    });
+
+
+    return divs;
+
+
+};
+
+console.log(gridMaker(document.querySelector("#grid"), valueRows, valueCols));
+
+
+let buttonC = document.querySelector("button");
+buttonC.addEventListener("click", function () {
+
+
+  console.log(gridMaker(document.querySelector("#grid"), valueRows, valueCols));
+
+
+});
+
+
+
+
+
+
+
+
+
+
